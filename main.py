86e42377a60c0e6ccd9f3306bfa631a19625eab7
Kirill K. <k.e.kalinin@gmail.com>
2023-11-23 21:33:00 +0300

    Добавил код и интерфейс

---
+++ main.py
@@ -0,0 +1,39 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.pushButton.clicked.connect(self.click)
+        self.do_paint = False
+
+    def click(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.risuem(qp)
+            qp.end()
+        self.do_paint = False
+
+    def risuem(self, qp):
+        r = random.randint(10, 200)
+
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(50, 50, 50 + r, 50 + r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
