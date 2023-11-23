8bcb1aaf68b9991795aa136ec65a919121d1fecf
Kirill K. <k.e.kalinin@gmail.com>
2023-11-23 21:44:04 +0300

    интерфейс в программе, случайный цвет

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import sys
 import random
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setGeometry(300, 300, 500, 500)
+        self.pushButton = QPushButton("Нарисовать", self)
         self.pushButton.clicked.connect(self.click)
         self.do_paint = False
 
@@ -27,8 +27,11 @@ class MyWidget(QMainWindow):
 
     def risuem(self, qp):
         r = random.randint(10, 200)
+        cr = random.randint(0, 255)
+        cg = random.randint(0, 255)
+        cb = random.randint(0, 255)
 
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(cr, cg, cb))
         qp.drawEllipse(50, 50, 50 + r, 50 + r)
 
 
